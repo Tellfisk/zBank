d575a247c7b3bc70c7164020df4c94acacaf217d
Tellfisk <tellev@outlook.com>
2021-02-24 15:24:27 +0100

    Folder restructure - To make VS structure match system structure

--- src/zBankAPI/Controllers/BankController.cs
+++ src/zBankAPI/Controllers/BankController.cs
@@ -3,11 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using zBankAPI.BusinessLogic;
 
 namespace zBankAPI.Controllers
 {
     public class BankController : ControllerBase
     {
-
+        BankBusinessLogic businessLogic = new BankBusinessLogic();
     }
 }
